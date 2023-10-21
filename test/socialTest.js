2938e41fd222a27543e50606e146b6afd1df4e8e
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-10-21 16:14:28 +0200

    6/11 10:20 Sinon Stubs (+ replace Twitter API by Reqres API)

--- test/socialTest.js
+++ test/socialTest.js
@@ -1,31 +1,50 @@
 // Tests libs
-const expect = require('chai').expect
+const chai = require('chai')
+const expect = chai.expect;
 const sinon = require('sinon')
+
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+
 
 //To test
 const Social = require('../lib/Social')
 
 describe("Social", () => {
-  const url = "https://hugocollin.com"
-
-  it('should have twitter_url', () => {
-    expect(Social).has.property("twitter_url")
+  it('should have reqres_url', () => {
+    expect(Social).has.property("reqres_url")
   });
 
   it('should have facebook_url', () => {
     expect(Social).has.property("facebook_url")
   });
 
-  describe('#getTwitterCount', () =>
+  describe('#getReqresCount', () =>
   {
     it('should be a function', () => {
-      expect(Social.getTwitterCount).a('function')
+      expect(Social.getReqresCount).a('function')
     });
 
     it('should call callAPI', () => {
       sinon.spy(Social, "callAPI")
-      Social.getTwitterCount(url)
-      expect(Social.callAPI.withArgs(Social.twitter_url + url).calledOnce).true
+      Social.getReqresCount()
+      expect(Social.callAPI.withArgs(Social.reqres_url).calledOnce).true
+      Social.callAPI.restore(); //Avoid conflicts between tests (e.g. stubs)
     })
+
+    it('should return count', (done) => {
+      const stub = sinon.stub(Social, 'callAPI')
+      // replace API by cutting function call to give the expected value
+      // stub.returns(new Promise((resolve, reject) => {
+      //   resolve({total:12})
+      // }))
+      stub.resolves({total:12})
+
+      // Social.getReqresCount().then((count) => {
+      //   expect(count).equal(12)
+      //   done()
+      // })
+      expect(Social.getReqresCount()).eventually.equal(12).notify(done); // replace above by using chai-as-promised
+    });
   });
 })
